422d72243967619fee8769d2bef76d22d1bc3022
anthonbrooks <51774498+anthonbrooks@users.noreply.github.com>
2026-02-04 15:47:46 -0700

    Add some CSS styles in Signup.css

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,22 +1,27 @@
+import './Signup.css'
+
 export default function Signup() {
     return (
-        <form>
+        <div>
 
-            <div>
+            <form>
 
-                <h1>Create An Account</h1>
+                    <h1>Create An Account</h1>
 
-                <input type="name" name="name" placeholder="Name"></input>
+                    <input type="name" name="name" class="input" placeholder="Name"></input>
 
-                <input type="email" name="email" placeholder="Email Address"></input>
+                    <input type="email" name="email" class="input" placeholder="Email Address"></input>
 
-                <input type="password" name="password" placeholder="Password"></input>
+                    <input type="password" name="password" class="input" placeholder="Password"></input>
 
-                <input type="password" name="password" placeholder="Confirm password"></input>
+                    <input type="password" name="password" class="input" placeholder="Confirm password"></input>
 
-                <button type="submit">Create Account</button>
+                    <button type="submit">Create Account</button>
 
-            </div>
-        </form>
+                    <p>Already Have An Account? <a href='#'>Sign In</a></p>
+
+            </form>
+
+        </div>
     )
 }
