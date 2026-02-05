52b38e6a53024992b541db4ed53f07bc69c59585
anthonbrooks <51774498+anthonbrooks@users.noreply.github.com>
2026-02-05 14:20:33 -0700

    Complete CSS design of Signup.jsx

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -4,23 +4,38 @@ export default function Signup() {
     return (
         <div>
 
+            <div class="nav-bar">
+
+                <ul>
+                    <li><a id='sign-in' href='#'>Sign In</a></li>
+                    <li><a id='create-mealtrain' href='#'>Create a Mealtrain</a></li>
+                </ul>
+            </div>
+
             <form>
 
-                    <h1>Create An Account</h1>
+                    <h1 id='create-account'>Create An Account</h1>
 
+                    <label id='name' for='name'>Name</label>
                     <input type="name" name="name" class="input" placeholder="Name"></input>
 
+                    <label id='email' for='email'>Email Address</label>
                     <input type="email" name="email" class="input" placeholder="Email Address"></input>
 
+                    <label id='password' for='password'>Password</label>
                     <input type="password" name="password" class="input" placeholder="Password"></input>
 
-                    <input type="password" name="password" class="input" placeholder="Confirm password"></input>
+                    <label id='confirm-password' for='confirm-password'>Confirm Password</label>
+                    <input type="password" name="password" class="input" placeholder="Confirm Password"></input>
 
                     <button type="submit">Create Account</button>
 
                     <p>Already Have An Account? <a href='#'>Sign In</a></p>
 
             </form>
+
+            <div id='rectangle-left'></div>
+            <div id='rectangle-right'></div>
 
         </div>
     )
