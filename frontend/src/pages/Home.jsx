b2ffb48768bf93c441f9bd762b104d60811e2693
Lore <lorevandenheede@gmail.com>
2026-02-05 14:50:27 +0100

    Add minimal Home component and integrate react-router-dom for routing

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+//import './Home.css'; 
+
+function Home() {
+    return (
+        <main className="home">
+        <section className="hero">
+        <h1>Showing up for people when life gets hard.</h1>
+        <button>Let's get started</button>
+        </section>
+        </main>
+
+    );
+}
+
+export default Home;
