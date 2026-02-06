79496068175d5db4d35af93ae7a01893b8e3101c
Lore <lorevandenheede@gmail.com>
2026-02-06 12:06:43 +0100

    Add UserDashboard basic styles and refactor layout structure

--- frontend/src/pages/UserDashboard.jsx
+++ frontend/src/pages/UserDashboard.jsx
@@ -1,9 +1,11 @@
+import './UserDashboard.css'
+
 export default function UserDashboard() {
     return (
         <div className="dashboard-container">
-            <asider className="dashboard-aide">
+            <aside className="dashboard-aside">
                 <img src="" alt="Avatar" />
-                <p>Welcome Anar!</p> Add the user's name
+                <p>Welcome Anar!</p>
                 <nav>
                     <ul>
                         <li>Dashboard</li>
@@ -12,21 +14,24 @@ export default function UserDashboard() {
                         <li>Create Meal Train</li>
                     </ul>
                     </nav>
-            </asider>
-            <main className="created-meal-trains">
-                <h1>Created Meal Trains</h1>
-                <button>Add</button>
-                <h2>Mihai's Meal Train</h2>
-                <p>Mihai is sick so i made this meal train.</p>
-                <h2>Lore's Meal Train</h2>
-                <p>Lore is sick so i made this meal train.</p>
+            </aside>
+            <main className="dashboard-content">
+                <section className="created-meal-trains">
+                    <h1>Created Meal Trains</h1>
+                    <button>Add</button>
+                    <h2>Mihai's Meal Train</h2>
+                    <p>Mihai is sick so i made this meal train.</p>
+                    <h2>Lore's Meal Train</h2>
+                    <p>Lore is sick so i made this meal train.</p>
+                </section>
+                <section className="joined-meal-trains">
+                    <h1>Joined Meal Trains</h1>
+                    <button>Join</button>
+                    <h2>Akshar's Meal Train</h2>
+                    <p>Akshar is sick so i made this meal train.</p>
+                </section>
             </main>
-            <main className="joined-meal-trains">
-                <h1>Joined Meal Trains</h1>
-                <button>Join</button>
-                <h2>Akshar's Meal Train</h2>
-                <p>Akshar is sick so i made this meal train.</p>
-            </main>
+
         </div>
     )
 }   
