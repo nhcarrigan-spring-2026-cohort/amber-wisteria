8fd7c6ff8b60283a5280612242bcdc9482947ea0
anthonbrooks <51774498+anthonbrooks@users.noreply.github.com>
2026-02-06 14:47:01 -0700

    Update class to className

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -4,7 +4,7 @@ export default function Signup() {
     return (
         <div>
 
-            <div class="nav-bar">
+            <div className="nav-bar">
 
                 <ul>
                     <li><a id='sign-in' href='#'>Sign In</a></li>
@@ -17,16 +17,16 @@ export default function Signup() {
                     <h1 id='create-account'>Create An Account</h1>
 
                     <label id='name' for='name'>Name</label>
-                    <input type="name" name="name" class="input" placeholder="Name"></input>
+                    <input type="name" name="name" className="input" placeholder="Name"></input>
 
                     <label id='email' for='email'>Email Address</label>
-                    <input type="email" name="email" class="input" placeholder="Email Address"></input>
+                    <input type="email" name="email" className="input" placeholder="Email Address"></input>
 
                     <label id='password' for='password'>Password</label>
-                    <input type="password" name="password" class="input" placeholder="Password"></input>
+                    <input type="password" name="password" className="input" placeholder="Password"></input>
 
                     <label id='confirm-password' for='confirm-password'>Confirm Password</label>
-                    <input type="password" name="password" class="input" placeholder="Confirm Password"></input>
+                    <input type="password" name="password" className="input" placeholder="Confirm Password"></input>
 
                     <button type="submit">Create Account</button>
 
