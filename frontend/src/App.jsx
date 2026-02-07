10ff36e59bcaf1fdbb6a520c943f8a5acdab9362
anthonbrooks <51774498+anthonbrooks@users.noreply.github.com>
2026-02-07 11:22:07 -0700

    Fix dark mode and add htmlFor on labels

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { Route, Routes } from 'react-router'
 import './App.css'
 import Signup from './pages/Signup'
 
@@ -10,15 +9,7 @@ function App() {
   
   return (
     <Routes>
-      {/* Note: Add routes to your page below (uncomment, edit or create new)
-          path -> location of your page, amber-wisteria/login 
-          element -> your page e.g, Login.jsx (don't forget to first import then use)*/}
-<Route path="/signup" element={<Signup />} />
-      {/* <Route index element={<Homepage />} />
-      <Route path='login' element={<LoginForm />} />
-      <Route path='signup' element={<Signup />} />
-      <Route path='user-dashboard' element={<UserDashboard />} />
-      <Route path='create-meal-train' element={<CreateMealForm />} /> */}
+      <Route path="/signup" element={<Signup />} />
     </Routes>
   )
 }
