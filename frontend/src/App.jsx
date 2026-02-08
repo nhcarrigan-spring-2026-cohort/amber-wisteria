3abe789a5b03fb6b529f614220caed50064774d7
Akanksha Negi <182057372+akankshauw@users.noreply.github.com>
2026-02-08 07:45:30 +0000

    fixed double router conflict

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,18 +4,15 @@ import './App.css'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { Route, Routes } from 'react-router'
 import Signup from './pages/Signup'
 
   
 function App() {
   return (
-    <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<div>Home Page - Coming Soon</div>} />
       </Routes>
-    </Router>
   )
 }
 
