29364ce515d7ead72232f06ab8994b12bbf0e50d
anthonbrooks <51774498+anthonbrooks@users.noreply.github.com>
2026-02-10 11:11:17 -0800

    Adjust size of bubbles for smaller screens

--- frontend/src/components/Background.jsx
+++ frontend/src/components/Background.jsx
@@ -3,9 +3,9 @@ import './Background.css'
 export default function Background({ children }) {
     return (
         
-        <div className="">
-            <div id='rectangle-left' className='absolute w-187.5 h-187.5 z-1 -top-60 -left-77.25 bg-[#F68300] -rotate-45 rounded-[160px]'></div>
-            <div id='rectangle-right' className='absolute w-187.5 h-187.5 z-1 -bottom-77.5 -right-77.25 bg-[#FEB058] -rotate-45 rounded-[160px]'></div>
+        <div className="w-full">
+            <div id='rectangle-left' className='absolute min-w-135 min-h-135 lg:w-187.5 lg:h-187.5 z-1 -top-60 -left-77.25 bg-[#F68300] -rotate-45 rounded-[160px] md:w-150 md:h-150 -sm:left-25'></div>
+            <div id='rectangle-right' className='absolute min-w-135 min-h-135 lg:w-187.5 lg:h-187.5 z-1 -bottom-77.5 -right-77.25 bg-[#FEB058] -rotate-45 rounded-[160px] md:w-150 md:h-150'></div>
             { children }
         </div>
     )
