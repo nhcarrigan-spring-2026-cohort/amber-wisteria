f3eac40337e669d43eb113d09ed1539ed0214077
JoaquinVeraOrtega <j.veraortega99@gmail.com>
2026-02-11 17:43:15 -0300

    add swagger docs config and urls

--- backend/user/views.py
+++ backend/user/views.py
@@ -2,6 +2,7 @@ from rest_framework import status
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from drf_yasg.utils import swagger_auto_schema
 
 from .serializers import (
     LoginSerializer,
@@ -13,7 +14,8 @@ from .serializers import (
 
 class RegisterView(APIView):
     permission_classes = [AllowAny]
-
+    
+    @swagger_auto_schema(request_body=RegisterSerializer)
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -26,6 +28,7 @@ class RegisterView(APIView):
 class LoginView(APIView):
     permission_classes = [AllowAny]
 
+    @swagger_auto_schema(request_body=LoginSerializer)
     def post(self, request):
         serializer = LoginSerializer(data=request.data)
         if not serializer.is_valid():
