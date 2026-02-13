23ab899b60a4203bd6006ef8245a1be75de65f3d
Muhammad Bilal Nasir <bilalnasireducation@gmail.com>
2026-02-13 23:12:20 +0500

    Added calendar component

---
+++ frontend/src/components/MealCalendar.jsx
@@ -0,0 +1,39 @@
+import Calendar from "react-calendar"
+
+export default function MealCalendar({
+    mode = "edit",
+    selectedDates,
+    activeDate,
+    onDayClick,
+    formatDate,
+}) {
+
+    const handleClick = (clickedDate) => {
+        const formatted = formatDate(clickedDate);
+
+        // view -> select only highlighted days (selected by meal train creator) to see meals
+        if (mode === "view") {
+            if (selectedDates[formatted]) {
+                onDayClick(clickedDate);
+            }
+            return;
+        }
+
+        // edit
+        onDayClick(clickedDate);
+    }
+
+    return (
+        <Calendar
+            onClickDay={handleClick}
+            tileClassName={({ date }) => {
+                const formatted = formatDate(date);
+
+                if (formatted === activeDate) return "active-day";
+                if (selectedDates[formatted]) return "selected-day";
+
+                return null;
+            }}
+        />
+    )
+}
