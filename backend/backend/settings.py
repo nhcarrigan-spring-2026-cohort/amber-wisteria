c4a584090706579d01c37b7026b3882754ebb46b
JoaquinVeraOrtega <j.veraortega99@gmail.com>
2026-02-13 20:28:47 -0300

    Add CORS headers support to Django settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -42,16 +42,22 @@ INSTALLED_APPS = [
     "user.apps.UserConfig",
    'drf_yasg',
     "mealTrain",
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
 ]
 
 ROOT_URLCONF = 'backend.urls'
