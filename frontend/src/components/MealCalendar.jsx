bdc98e3bd9b943f12cd4540b07915daad571d5e3
Muhammad Bilal Nasir <bilalnasireducation@gmail.com>
2026-02-13 23:28:15 +0500

    Prettier bug fix

--- frontend/src/components/MealCalendar.jsx
+++ frontend/src/components/MealCalendar.jsx
@@ -1,39 +1,38 @@
-import Calendar from "react-calendar"
+import Calendar from 'react-calendar';
 
 export default function MealCalendar({
-    mode = "edit",
-    selectedDates,
-    activeDate,
-    onDayClick,
-    formatDate,
+  mode = 'edit',
+  selectedDates,
+  activeDate,
+  onDayClick,
+  formatDate
 }) {
+  const handleClick = (clickedDate) => {
+    const formatted = formatDate(clickedDate);
 
-    const handleClick = (clickedDate) => {
-        const formatted = formatDate(clickedDate);
-
-        // view -> select only highlighted days (selected by meal train creator) to see meals
-        if (mode === "view") {
-            if (selectedDates[formatted]) {
-                onDayClick(clickedDate);
-            }
-            return;
-        }
-
-        // edit
+    // view -> select only highlighted days (selected by meal train creator) to see meals
+    if (mode === 'view') {
+      if (selectedDates[formatted]) {
         onDayClick(clickedDate);
+      }
+      return;
     }
 
-    return (
-        <Calendar
-            onClickDay={handleClick}
-            tileClassName={({ date }) => {
-                const formatted = formatDate(date);
+    // edit
+    onDayClick(clickedDate);
+  };
 
-                if (formatted === activeDate) return "active-day";
-                if (selectedDates[formatted]) return "selected-day";
+  return (
+    <Calendar
+      onClickDay={handleClick}
+      tileClassName={({ date }) => {
+        const formatted = formatDate(date);
 
-                return null;
-            }}
-        />
-    )
+        if (formatted === activeDate) return 'active-day';
+        if (selectedDates[formatted]) return 'selected-day';
+
+        return null;
+      }}
+    />
+  );
 }
