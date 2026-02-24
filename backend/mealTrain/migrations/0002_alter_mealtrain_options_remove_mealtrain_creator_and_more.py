64020dc93fd86e3ba03939d2c6e5e41ea971f80e
Muhammad Bilal Nasir <bilalnasireducation@gmail.com>
2026-02-24 23:28:15 +0500

    Fix null organizer bug

--- backend/mealTrain/migrations/0002_alter_mealtrain_options_remove_mealtrain_creator_and_more.py
+++ backend/mealTrain/migrations/0002_alter_mealtrain_options_remove_mealtrain_creator_and_more.py
@@ -66,7 +66,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='mealtrain',
             name='organizer',
-            field=models.ForeignKey(default='', on_delete=django.db.models.deletion.CASCADE, related_name='organized_meal_trains', to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='organized_meal_trains', to=settings.AUTH_USER_MODEL),
             preserve_default=False,
         ),
         migrations.AlterField(
